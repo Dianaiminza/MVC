425b11992930c1be48e3c175c5497aaac062aa84
diana.iminza <diana.iminza@ronforddigital.com>
2022-03-24 08:11:17 +0300

    first commit

---
+++ MvcBM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcBM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
